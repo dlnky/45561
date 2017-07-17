47108b5f00dc250b11b720dbe254e3ef7293004c
Cole Greenwood <cmg7289@rit.edu>
2017-07-16 21:51:55 -0700

    Problem 9 assignment 5 45561
    Also made changes to problem 5

---
+++ Hmwk/Savitch_9thEd_Chap5_Prob9_TrianglePerim/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Cole Greenwood
+ * Created on July 10, 2017, 3:06 PM
+ * Purpose:  Calculate the annual mortgage of a house post-tax
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+
+
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    
+    //Exit
+    return 0;
+}
